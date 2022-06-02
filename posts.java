a0947a8713ca105d61ac08c11b1e8081dc72931a
Osminalx <verdestro64@gmail.com>
2022-06-02 15:31:00 -0700

    arreglos pequeños

---
+++ posts.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+public class posts extends JFrame {
+    public posts(/* aqui van los argumentos para revizar si es usuario o invitado */) {
+        setSize(500, 500);
+
+    }
+
+}
