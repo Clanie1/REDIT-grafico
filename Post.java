dc0adbe7464954281f50c5144a325c4756fd8736
Osminalx <verdestro64@gmail.com>
2022-06-02 21:12:50 -0700

    file writer, falta configurar nombre para que lo tomé del usuarios.daniel

--- Post.java
+++ Post.java
@@ -9,9 +9,16 @@ public class Post {
     String titulo;
     String contenido;
 
+    public String user = ".//docs//usuarios.daniel";
     Scanner scn;
 
     FileWriter fw;
-    public String datos = "";
+    public String datos = ".//docs//publicaciones.daniel";
     File file = new File(datos);
+
+    public void WritePost() throws IOException {
+        fw = new FileWriter(file, true);
+        fw.write(nombre + "¬" + hora + "¬" + titulo + "¬" + contenido + "\n");
+        fw.close();
+    }
 }
