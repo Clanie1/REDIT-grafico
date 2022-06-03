1f1e726f0930ce279a6a3d54f86d46c6759d49ea
el-shago <sperezmaris@gmail.com>
2022-06-02 21:34:28 -0700

    cambiosfacheritos
    juju

--- screens/principal.java
+++ screens/principal.java
@@ -10,9 +10,21 @@ public class principal extends JFrame {
         setSize(875, 625);
         setLayout(null);
 
-        JLabel pst = new JLabel();
-        pst.setBounds(25, 50, 800, 400);
-        add(pst);
+        JLabel nombre = new JLabel();
+        nombre.setBounds(25, 50, 100, 25);
+        add(nombre);
+
+        JLabel hora = new JLabel();
+        hora.setBounds(200, 50, 100, 25);
+        add(hora);
+
+        JLabel titulito = new JLabel();
+        titulito.setBounds(25, 125, 150, 25);
+        add(titulito);
+
+        JLabel contenido = new JLabel();
+        contenido.setBounds(25, 200, 700, 250);
+        add(contenido);
 
         JButton nxt = new JButton("Next Post");
         nxt.setBounds(705, 480, 120, 25);
@@ -23,5 +35,38 @@ public class principal extends JFrame {
         add(entr);
         setVisible(true);
     }
+    
+     public principal(User user) {
+         setSize(875, 625);
+         setLayout(null);
 
+         JLabel usuarioguapo = new JLabel();
+         usuarioguapo.setBounds(25, 10, 100, 25);
+         add(usuarioguapo);
+
+         JLabel nombre = new JLabel();
+         nombre.setBounds(25, 50, 100, 25);
+         add(nombre);
+
+         JLabel hora = new JLabel();
+         hora.setBounds(200, 50, 100, 25);
+         add(hora);
+
+         JLabel titulito = new JLabel();
+         titulito.setBounds(25, 125, 150, 25);
+         add(titulito);
+
+         JLabel contenido = new JLabel();
+         contenido.setBounds(25, 200, 700, 250);
+         add(contenido);
+
+         JButton nxt = new JButton("Next Post");
+         nxt.setBounds(705, 480, 120, 25);
+         add(nxt);
+
+         JButton entr = new JButton("Exit Account");
+         entr.setBounds(25, 480, 120, 25);
+         add(entr);
+         setVisible(true);
+     }
 }
